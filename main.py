001c3dd9de594060bee4a789bf890fbe40a963c9
Giovanni <ivanalipov4@gmail.com>
2025-10-30 21:37:01 +0100

    2

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end=" ")
+
+    print()
+    for value in kwargs.values():
+        print(value, end="\n")
+
+shipping_label("Dr.", "Spongebob", "Squarepants", "III", street="123 Fake str.", apartment="100", state="MI")
