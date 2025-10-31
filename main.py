1beae8f4480084178036cb800558208f9c7e8bc6
Giovanni <ivanalipov4@gmail.com>
2025-10-31 21:25:48 +0100

    4

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-def add_sprinkles(func):
-    def wrapper(*args, **kwargs):
-        print("*You add sprinkles*")
-        func(*args, **kwargs)
-    return wrapper
-
-def add_fudge(func):
-    def wrapper(*args, **kwargs):
-        print("*You add fudge*")
-        func(*args, **kwargs)
-    return wrapper
-
-@add_sprinkles
-@add_fudge
-def get_ice_cream(flavor):
-    print(f"Here is your {flavor} ice cream!")
-
-get_ice_cream("chocolate")
+print("67")
